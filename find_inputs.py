41ff97224e3fa6f6977211a5ddd5c002b77450a9
“Sumanth <sumanthgenz@gmail.com>
2020-11-29 21:46:12 -0800

    inputs, outputs in AG-format, locator of inputs

---
+++ find_inputs.py
@@ -0,0 +1,16 @@
+import random
+import glob
+import json
+import filecmp 
+
+
+if __name__ == "__main__":
+    targets = ["proj-inputs/50.in", "proj-inputs/20-cluster.in"]
+    matches = []
+    for path in glob.glob('inputs/*'): 
+        for t in targets:
+            if filecmp.cmp(t, path): 
+                matches.append((t, path))
+
+    with open('found_inputs.json', 'w') as f:
+        f.write(json.dumps(matches))
