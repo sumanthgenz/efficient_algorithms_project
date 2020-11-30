69a5a358f4ebd4902ed5a1999e22f745b034ef70
Sumanth G <sumanthgenz@gmail.com>
2020-11-30 11:47:35 -0800

    Create ensemble.py

---
+++ src/ensemble.py
@@ -0,0 +1,18 @@
+import random
+import glob
+from sklearn.cluster import KMeans
+import numpy as np
+
+from cluster_algo import main cluster_main
+from greedy_algo import main as greedy_main
+
+
+if __name__ == "__main__":
+	H = None
+	S = None
+	N_samples = 10
+	
+	#return max(greedy_main(**args), cluster_main(H, S, N_samples))
+
+
+
