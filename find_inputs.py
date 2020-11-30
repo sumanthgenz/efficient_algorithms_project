3e58fddb746aa97468bc6daded670b6b54558536
“Sumanth <sumanthgenz@gmail.com>
2020-11-30 01:42:11 -0800

    located our remaining inputs

--- find_inputs.py
+++ find_inputs.py
@@ -5,7 +5,7 @@ import filecmp
 
 
 if __name__ == "__main__":
-    targets = ["proj-inputs/50.in", "proj-inputs/20-cluster.in"]
+    targets = ["proj-inputs/50.in", "proj-inputs/20.in", "proj-inputs/10.in"]
     matches = []
     for path in glob.glob('inputs/*'): 
         for t in targets:
